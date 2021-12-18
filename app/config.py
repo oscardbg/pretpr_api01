0d98860e7b498b6009d1c221d3f89151fd6f0021
Bruce Wayne <brucewayne@mail.com>
2021-12-18 12:30:35 -0500

    Models and db creation

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 class Config:
 	DEBUG = True
 	SECRET_KEY = 'klj23l4kj23lk42lk3j'
-	
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///apidata.db'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ app/models.py
@@ -0,0 +1,21 @@
+from app import db
+
+class User(db.Model):
+	id = db.Columnd(db.Integer, primary_key=True)
+	public_id = db.Columnd(db.String(100), unique=True)
+	name = db.Column(db.String(100))
+	password = db.Column(db.String(100))
+	admin = db.Column(db.Boolean)
+	todos = db.relationship('Todo', backref='user')
+
+	def __str__(self):
+		return f'{self.id}, {self.name} '
+
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	body = db.Column(db.String(150))
+	complete = db.Column(db.Boolean)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __str__(self):
+		return f'Todo {self.id} '
